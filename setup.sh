18f4249fcbb4b7b95cbd4922835aebb7b669b8bf
Anang11042000 <72434392+Anang11042000@users.noreply.github.com>
2023-09-01 03:46:32 +0700

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -445,90 +445,92 @@ wget -q https://raw.githubusercontent.com/Anang11042000/kucing/main/backup/set-b
 
 # // Download Data
 echo -e "${GREEN}Download Data${NC}"
-
-wget -q -O /usr/bin/usernew "https://raw.githubusercontent.com/Anang11042000/kucing/main/usernew.sh"
-wget -q -O /usr/bin/add-ws "https://raw.githubusercontent.com/Anang11042000/kucing/main/add-ws.sh"
-wget -q -O /usr/bin/add-ssws "https://raw.githubusercontent.com/Anang11042000/kucing/main/add-ssws.sh"
-wget -q -O /usr/bin/add-vless "https://raw.githubusercontent.com/Anang11042000/kucing/main/add-vless.sh"
-wget -q -O /usr/bin/add-tr "https://raw.githubusercontent.com/Anang11042000/kucing/main/add-tr.sh"
-wget -q -O /usr/bin/autoreboot "https://raw.githubusercontent.com/Anang11042000/kucing/main/options/autoreboot.sh"
-wget -q -O /usr/bin/restart "https://raw.githubusercontent.com/Anang11042000/kucing/main/options/restart.sh"
-wget -q -O /usr/bin/tendang "https://raw.githubusercontent.com/Anang11042000/kucing/main/options/tendang.sh"
-wget -q -O /usr/bin/clearlog "https://raw.githubusercontent.com/Anang11042000/kucing/main/options/clearlog.sh"
-wget -q -O /usr/bin/running "https://raw.githubusercontent.com/Anang11042000/kucing/main/options/running.sh"
-#wget -q -O /usr/bin/cek-trafik "https://raw.githubusercontent.com/Anang11042000/kucing/main/options/cek-trafik.sh"
-wget -q -O /usr/bin/speedtest "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/speedtest_cli.py"
-wget -q -O /usr/bin/cek-bandwidth "https://raw.githubusercontent.com/Anang11042000/kucing/main/options/cek-bandwidth.sh"
-wget -q -O /usr/bin/limitspeed "https://raw.githubusercontent.com/Anang11042000/kucing/main/options/limitspeed.sh"
-wget -q -O /usr/bin/menu-vless "https://raw.githubusercontent.com/Anang11042000/kucing/main/menu/menu-vless.sh"
-wget -q -O /usr/bin/menu-vmess "https://raw.githubusercontent.com/Anang11042000/kucing/main/menu/menu-vmess.sh"
-wget -q -O /usr/bin/menu-ss "https://raw.githubusercontent.com/Anang11042000/kucing/main/menu/menu-ss.sh"
-wget -q -O /usr/bin/menu-trojan "https://raw.githubusercontent.com/Anang11042000/kucing/main/menu/menu-trojan.sh"
-wget -q -O /usr/bin/menu-ssh "https://raw.githubusercontent.com/Anang11042000/kucing/main/menu/menu-ssh.sh"
-wget -q -O /usr/bin/menu-backup "https://raw.githubusercontent.com/Anang11042000/kucing/main/menu/menu-backup.sh"
-wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/Anang11042000/kucing/main/menu/menu.sh"
-wget -q -O /usr/bin/webmin "https://raw.githubusercontent.com/Anang11042000/kucing/main/options/webmin.sh"
-wget -q -O /usr/bin/xp "https://raw.githubusercontent.com/Anang11042000/kucing/main/xp.sh"
-wget -q -O /usr/bin/update "https://raw.githubusercontent.com/Anang11042000/kucing/main/options/update.sh"
-wget -q -O /usr/bin/addhost "https://raw.githubusercontent.com/Anang11042000/kucing/main/menu/addhost.sh"
-wget -q -O /usr/bin/certxray "https://raw.githubusercontent.com/Anang11042000/kucing/main/menu/crt.sh"
-wget -q -O /usr/bin/menu-set "https://raw.githubusercontent.com/Anang11042000/kucing/main/menu/menu-set.sh"
-wget -q -O /usr/bin/info "https://raw.githubusercontent.com/Anang11042000/kucing/main/options/info.sh"
-wget -q -O /usr/bin/infoserv "https://raw.githubusercontent.com/Anang11042000/kucing/main/options/infoserv.sh"
-wget -q -O /usr/bin/about "https://raw.githubusercontent.com/Anang11042000/kucing/main/options/about.sh"
-wget -q -O /usr/bin/sslh-fix-reboot "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/sslh-fix/sslh-fix-reboot.sh"
-wget -q -O /usr/bin/mesinssh "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/mesinssh.sh"
-wget -q -O /usr/bin/cek-ssh "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/cek-ssh.sh"
-wget -q -O /usr/bin/loop "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/loop.sh"
-wget -q -O /usr/bin/matikan "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/matikan.sh"
-wget -q -O /usr/bin/limit "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/limit.sh"
-wget -q -O /usr/bin/limit-ip-ssh "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/limit-ip-ssh.sh"
-wget -q -O /usr/bin/limit-ip-trojan "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/limit-ip-trojan.sh"
-wget -q -O /usr/bin/limit-ip-vless "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/limit-ip-vless.sh"
-wget -q -O /usr/bin/limit-ip-vmess "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/limit-ip-vmess.sh"
-wget -q -O /usr/bin/limit-quota-trojan "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/limit-quota-trojan.sh"
-wget -q -O /usr/bin/limit-quota-vmess "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/limit-quota-vmess.sh"
-wget -q -O /usr/bin/limit-quota-vless "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/limit-quota-vless.sh"
-wget -q -O /usr/bin/quota "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/quota.sh"
-wget -q -O /usr/bin/quota-trojan-grpc "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/quota-trojan-grpc.sh"
-wget -q -O /usr/bin/quota-trojan-ws "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/quota-trojan-ws.sh"
-wget -q -O /usr/bin/quota-vmess-grpc "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/quota-vmess-grpc.sh"
-wget -q -O /usr/bin/quota-vmess-ws "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/quota-vmess-ws.sh"
-wget -q -O /usr/bin/quota-vless-ws "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/quota-vless-ws.sh"
-wget -q -O /usr/bin/quota-vless-grpc "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/quota-vless-grpc.sh"
-wget -q -O /usr/bin/quota-vmess-orbit "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/quota-vmess-ws-orbit.sh"
-wget -q -O /usr/bin/quota-vmess-orbit1 "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/quota-vmess-ws-orbit1.sh"
-chmod +x /usr/bin/usernew
-chmod +x /usr/bin/add-ws
-chmod +x /usr/bin/add-ssws
-chmod +x /usr/bin/add-vless
-chmod +x /usr/bin/add-tr
-chmod +x /usr/bin/autoreboot
-chmod +x /usr/bin/restart
-chmod +x /usr/bin/tendang
-chmod +x /usr/bin/clearlog
-chmod +x /usr/bin/running
-chmod +x /usr/bin/speedtest
-chmod +x /usr/bin/cek-bandwidth
-chmod +x /usr/bin/limitspeed
-chmod +x /usr/bin/menu-vless
-chmod +x /usr/bin/menu-vmess
-chmod +x /usr/bin/menu-ss
-chmod +x /usr/bin/menu-trojan
-chmod +x /usr/bin/menu-ssh
-chmod +x /usr/bin/menu-backup
-chmod +x /usr/bin/menu
-chmod +x /usr/bin/webmin
-chmod +x /usr/bin/xp
-chmod +x /usr/bin/update
-chmod +x /usr/bin/addhost
-chmod +x /usr/bin/certxray
-chmod +x /usr/bin/menu-set
-chmod +x /usr/bin/info
-chmod +x /usr/bin/infoserv
-chmod +x /usr/bin/about
-chmod +x /usr/bin/mesinssh
-chmod +x /usr/bin/cek-ssh
+cd /usr/bin
+wget -O usernew "https://raw.githubusercontent.com/Anang11042000/kucing/main/usernew.sh"
+wget -O add-ws "https://raw.githubusercontent.com/Anang11042000/kucing/main/add-ws.sh"
+wget -O add-ssws "https://raw.githubusercontent.com/Anang11042000/kucing/main/add-ssws.sh"
+wget -O add-vless "https://raw.githubusercontent.com/Anang11042000/kucing/main/add-vless.sh"
+wget -O add-tr "https://raw.githubusercontent.com/Anang11042000/kucing/main/add-tr.sh"
+wget -O autoreboot "https://raw.githubusercontent.com/Anang11042000/kucing/main/options/autoreboot.sh"
+wget -O restart "https://raw.githubusercontent.com/Anang11042000/kucing/main/options/restart.sh"
+wget -O tendang "https://raw.githubusercontent.com/Anang11042000/kucing/main/options/tendang.sh"
+wget -O clearlog "https://raw.githubusercontent.com/Anang11042000/kucing/main/options/clearlog.sh"
+wget -O running "https://raw.githubusercontent.com/Anang11042000/kucing/main/options/running.sh"
+#wget -O cek-trafik "https://raw.githubusercontent.com/Anang11042000/kucing/main/options/cek-trafik.sh"
+wget -O speedtest "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/speedtest_cli.py"
+wget -O cek-bandwidth "https://raw.githubusercontent.com/Anang11042000/kucing/main/options/cek-bandwidth.sh"
+wget -O limitspeed "https://raw.githubusercontent.com/Anang11042000/kucing/main/options/limitspeed.sh"
+wget -O menu-vless "https://raw.githubusercontent.com/Anang11042000/kucing/main/menu/menu-vless.sh"
+wget -O menu-vmess "https://raw.githubusercontent.com/Anang11042000/kucing/main/menu/menu-vmess.sh"
+wget -O menu-ss "https://raw.githubusercontent.com/Anang11042000/kucing/main/menu/menu-ss.sh"
+wget -O menu-trojan "https://raw.githubusercontent.com/Anang11042000/kucing/main/menu/menu-trojan.sh"
+wget -O menu-ssh "https://raw.githubusercontent.com/Anang11042000/kucing/main/menu/menu-ssh.sh"
+wget -O menu-backup "https://raw.githubusercontent.com/Anang11042000/kucing/main/menu/menu-backup.sh"
+wget -O menu "https://raw.githubusercontent.com/Anang11042000/kucing/main/menu/menu.sh"
+wget -O webmin "https://raw.githubusercontent.com/Anang11042000/kucing/main/options/webmin.sh"
+wget -O xp "https://raw.githubusercontent.com/Anang11042000/kucing/main/xp.sh"
+wget -O update "https://raw.githubusercontent.com/Anang11042000/kucing/main/options/update.sh"
+wget -O addhost "https://raw.githubusercontent.com/Anang11042000/kucing/main/menu/addhost.sh"
+wget -O certxray "https://raw.githubusercontent.com/Anang11042000/kucing/main/menu/crt.sh"
+wget -O menu-set "https://raw.githubusercontent.com/Anang11042000/kucing/main/menu/menu-set.sh"
+wget -O info "https://raw.githubusercontent.com/Anang11042000/kucing/main/options/info.sh"
+wget -O infoserv "https://raw.githubusercontent.com/Anang11042000/kucing/main/options/infoserv.sh"
+wget -O about "https://raw.githubusercontent.com/Anang11042000/kucing/main/options/about.sh"
+wget -O sslh-fix-reboot "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/sslh-fix/sslh-fix-reboot.sh"
+wget -O mesinssh "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/mesinssh.sh"
+wget -O cek-ssh "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/cek-ssh.sh"
+wget -O loop "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/loop.sh"
+wget -O matikan "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/matikan.sh"
+wget -O limit "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/limit.sh"
+wget -O limit-ip-ssh "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/limit-ip-ssh.sh"
+wget -O limit-ip-trojan "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/limit-ip-trojan.sh"
+wget -O limit-ip-vless "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/limit-ip-vless.sh"
+wget -O limit-ip-vmess "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/limit-ip-vmess.sh"
+wget -O limit-quota-trojan "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/limit-quota-trojan.sh"
+wget -O limit-quota-vmess "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/limit-quota-vmess.sh"
+wget -O limit-quota-vless "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/limit-quota-vless.sh"
+wget -O quota "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/quota.sh"
+wget -O quota-trojan-grpc "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/quota-trojan-grpc.sh"
+wget -O quota-trojan-ws "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/quota-trojan-ws.sh"
+wget -O quota-vmess-grpc "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/quota-vmess-grpc.sh"
+wget -O quota-vmess-ws "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/quota-vmess-ws.sh"
+wget -O quota-vless-ws "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/quota-vless-ws.sh"
+wget -O quota-vless-grpc "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/quota-vless-grpc.sh"
+wget -O quota-vmess-orbit "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/quota-vmess-ws-orbit.sh"
+wget -O quota-vmess-orbit1 "https://raw.githubusercontent.com/Anang11042000/kucing/main/tools/limit/quota-vmess-ws-orbit1.sh"
+chmod +x *
+chmod +x usernew
+chmod +x add-ws
+chmod +x add-ssws
+chmod +x add-vless
+chmod +x add-tr
+chmod +x autoreboot
+chmod +x restart
+chmod +x tendang
+chmod +x clearlog
+chmod +x running
+chmod +x speedtest
+chmod +x cek-bandwidth
+chmod +x limitspeed
+chmod +x menu-vless
+chmod +x menu-vmess
+chmod +x menu-ss
+chmod +x menu-trojan
+chmod +x menu-ssh
+chmod +x menu-backup
+chmod +x menu
+chmod +x webmin
+chmod +x xp
+chmod +x update
+chmod +x addhost
+chmod +x certxray
+chmod +x menu-set
+chmod +x info
+chmod +x infoserv
+chmod +x about
+chmod +x mesinssh
+chmod +x cek-ssh
+cd
 
 #cat > /etc/cron.d/re_otm <<-END
 #SHELL=/bin/sh
