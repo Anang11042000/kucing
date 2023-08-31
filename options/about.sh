e1c14c7139f0b3dd00d1d43779527f6061cf489a
anang11042000 <anangq928@gmail.com>
2023-09-01 00:07:08 +0700

    first commit

---
+++ options/about.sh
@@ -0,0 +1,21 @@
+ echo -e " ANANG " | lolcat
+
+
+echo -e "┌─────────────────────────────────────────────────┐" | lolcat
+echo -e "│               AUTOSCRIPT PREMIUM                │" | lolcat
+echo -e "│             BY ANANG VPN              │" | lolcat
+echo -e "└─────────────────────────────────────────────────┘" | lolcat
+
+echo -e "> Support OS 
+  For Ubuntu 18.04 & Ubuntu 20.04 & Ubuntu 22.04 
+  For Debian 9 & Debian 10 & Debian 11 " | lolcat
+echo ""
+echo -e "  Contact Owner AutoScript
+> WA: 087793993385
+> TELEGRAM: @anang_fatkhurosidin" | lolcat
+echo ""
+echo -e "Thanks for your Order ANANG VPN" | lolcat
+echo ""
+read -n 1 -s -r -p "  Press any key to Back to Menu"
+menu
+fi
